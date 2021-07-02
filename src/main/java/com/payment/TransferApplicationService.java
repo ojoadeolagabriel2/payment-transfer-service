0ec761581b3d21e70ad1d3a2a194df44a94fa0d7
Adeola <ojoadeolagabriel@gmail.com>
2021-07-02 09:23:22 +0100

    :sparkles: small refactor and do updates 2

--- src/main/java/com/payment/TransferApplicationService.java
+++ src/main/java/com/payment/TransferApplicationService.java
@@ -4,8 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class PaymentApplicationService {
+public class TransferApplicationService {
     public static void main(String[] args) {
-        SpringApplication.run(PaymentApplicationService.class, args);
+        SpringApplication.run(TransferApplicationService.class, args);
     }
 }
